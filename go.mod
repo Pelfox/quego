fc0135f46fe6e2e940eca597939f65849377c497
Aleksey Demchenkov <contact@pelfox.dev>
2025-10-02 08:47:37 +0300

    fix(database): embed migrations into the binary

--- go.mod
+++ go.mod
@@ -9,6 +9,8 @@ require (
 	github.com/labstack/echo/v4 v4.13.4
 	github.com/labstack/gommon v0.4.2
 	github.com/mattn/go-sqlite3 v1.14.32
+	github.com/redis/go-redis/v9 v9.14.0
+	github.com/rs/zerolog v1.34.0
 )
 
 require (
@@ -18,8 +20,6 @@ require (
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/redis/go-redis/v9 v9.14.0 // indirect
-	github.com/rs/zerolog v1.34.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.38.0 // indirect
--- internal/database.go
+++ internal/database.go
@@ -1,27 +1,38 @@
 package internal
 
 import (
+	"embed"
+	"errors"
 	"fmt"
 
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/sqlite3"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
 )
 
 // DatabaseFile is the default filename for the SQLite database used by the
 // server.
 const DatabaseFile = "quego.db"
 
+//go:embed migrations/*.sql
+var migrationsFS embed.FS
+
 // MigrateDatabase applies database schema migrations.
 func MigrateDatabase() error {
-	migrations, err := migrate.New(
-		"file://migrations", // where the migration files are located
+	data, err := iofs.New(migrationsFS, "migrations")
+	if err != nil {
+		return err
+	}
+	migrations, err := migrate.NewWithSourceInstance(
+		"iofs",
+		data,
 		fmt.Sprintf("sqlite3://%s", DatabaseFile),
 	)
 	if err != nil {
 		return fmt.Errorf("failed to initialize migrations: %w", err)
 	}
-	if err := migrations.Up(); err != nil && err != migrate.ErrNoChange {
+	if err := migrations.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
 		return fmt.Errorf("failed to apply migrations: %w", err)
 	}
 	return nil
